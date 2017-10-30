2868a88985468c362cc3c03eb7d90f116a87625a
stacyweter <stacyweter@yahoo.com>
2017-10-30 06:00:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const chalk = require('chalk');
+
+console.log(chalk.bgBlackBright.bold("Stacy's Chalk Project"));
+
+console.log(chalk.green.inverse("This is a story about a small pumpkin who was scared of Halloween."));
+console.log(chalk.green("Jester was a tiny pumpkin, the smallest pumpkin in the pumpkin patch."));
+console.log(chalk.green("While the other pumpkins were looking forward to Halloween,"));
+console.log(chalk.green("Jester was nervous."));
+console.log(chalk.green("Monsters, ghosts, and witches did not sound like fun to him."));
+console.log(chalk.green("He hoped that with his small size nobody would want him and he could stay in the pumpkin patch."));
+console.log(chalk.green("Then one day, he met Joey, a small boy who also didn't like the scary part of Halloween."));
+console.log(chalk.green("The minute he saw Jester he knew he had found the perfect pumpkin."));
+console.log(chalk.green("He told Jester that he wanted to decorate him with a happy face"));
+console.log(chalk.green("and that there would be no scary monsters, ghosts, or witches at his house."));
+console.log(chalk.green("Jester was happy to find the perfect Halloween home."));
+console.log(chalk.green("Both Joey and Jester enjoyed their non-scary Halloween."));
+console.log(chalk.bgGreenBright("The End."));
+
